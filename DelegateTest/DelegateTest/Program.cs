1d8ee5dbd0edc6ad3a19fee56a096a4f5876b021
김용찬 <ycbalor@gmail.com>
2019-03-09 13:56:48 +0900

    델리게이트 테스트 2

--- DelegateTest/DelegateTest/Program.cs
+++ DelegateTest/DelegateTest/Program.cs
@@ -8,25 +8,30 @@ namespace DelegateTest
 {
 	class Program
 	{
-		// 델리게이트는 총 3단계
-		private delegate int onjSum(int i, int j); // 1단계 선언
-		// 위의 델리게이트는 int를 반환하고 int 파라미터 2개를 받는 메소드가 호출될꺼라는 것을 짐작할 수 있음
+		// 델리게이트를 위해서 C#에서 Func와 Action이 있음
+		// Func는 반환값이 있는 것, Action은 반환값이 없는것 
+		// 둘다 인자를 15개 ~ 16개 까지 넣을 수 있다.
 
 		static void Main(string[] args)
 		{
 			// 2단계 생성, 메소드 이름을 인자로 넣는다. 
-			// 메소드 인자의 생긴것과 델리게이트는 생긴것이 동일해야함
 
-			onjSum myMethod = new onjSum(Program.Sum);
-			//onjSum myMethod = new onjSum(Sum); // 같은 static이면 메소드 이름만 인자로 넣어주어도 된다.
-			//onjSum myMethod = Sum; // 메소드 이름만 넣어줘도 된다.
+			Func<int, int, int> myMethod = Sum1;
+			Action<int, int> myMethod2 = Sum2;
 
 			Console.WriteLine("두수 합 : {0}", myMethod(10, 30)); // 3단계 실행
+			myMethod2(10, 20);
+
 		}
 
-		static int Sum(int i, int j)
+		static int Sum1(int i, int j)
 		{
 			return i + j;
+		}
+
+		static void Sum2(int i, int j)
+		{
+			Console.WriteLine(i + j);
 		}
 	}
 }
